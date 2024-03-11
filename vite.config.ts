ac7facdb9c1f777b4371fe3fa729ef94d1fd77c0
Kameer Hosein <kameer-hosien@outlook.com>
2024-03-10 20:34:59 -0400

    changged base location

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base : "/Neon-Technologies-Limited-Portfolio-Website",
+  base : "/Neon-Technologies-Limited-Portfolio-Website/",
   plugins: [react()],
 })
