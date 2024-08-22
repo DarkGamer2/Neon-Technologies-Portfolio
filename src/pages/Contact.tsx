c4bb4ce17b5beaa8a6ea637b085524ad91cd917c
Kameer Hosein <kameer-hosien@outlook.com>
2024-08-21 21:17:30 -0400

    added links to projects

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,7 +1,7 @@
 import ContactForm from "../components/ContactForm";
 import Footer from "../components/Footer";
 import NavigationBar from "../components/NavigationBar";
-
+import React from "react";
 const Contact = () => {
   return (
     <>
